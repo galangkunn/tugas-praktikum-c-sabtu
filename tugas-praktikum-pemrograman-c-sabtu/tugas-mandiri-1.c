eb6afb1c4fc748f3a7cc83c2c4e62a8397ffcaa6
galangkunn <putragalang727@gmail.com>
2024-09-26 18:42:26 +0700

    first commit

---
+++ tugas-praktikum-pemrograman-c-sabtu/tugas-mandiri-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("NIM : 2441015 \n");
+    printf("Nama  : Galang Putra Pratama \n");
+    printf("Prodi : Teknik Informatika");
+    return 0;
+}
---
+++ tugas-praktikum-pemrograman-c-sabtu/tugas-mandiri-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    //disini untuk mendeklarasikan panjang lebar keliling dan luas persegi panjang
+    int p,l,k,lu;
+    p = 10;
+    l = 7;
+    k = (p+l)*2;
+    lu = p*l;
+    //Disini Untuk menampilkan Panjang Lebar dan luasnya
+    printf("Panjang Persegi Panjang Tersebut Adalah %d cm \n",p);
+    printf("Lebar Persegi Panjang Tersebut Adalah %d cm \n",l);
+    printf("Keliling Persegi Panjang Tersebut Adalah %d cm \n",k);
+    printf("Luas Persegi Panjang Tersebut Adalah %d cm2",lu);
+    return 0;
+}
